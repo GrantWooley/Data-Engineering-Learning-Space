832b376a2e09603e7b3283ac0367c8b9119c6e8a
Grant Wooley <grantW900@yahoo.com>
2024-07-15 18:43:43 -0500

    Starting to work on Hacker Rank list comperhension problem

--- LearningFile.py
+++ LearningFile.py
@@ -4,3 +4,38 @@ File used for going through courses, learning material, questions, etc...
 """
 
 # This is the file ill be using for working on list comprehension today.
+
+
+#Working on the hack rank list copmrehension problem now.
+#[itemtoadd for item in iterable if condition ==True ]
+
+# i can be between 0 and x
+#is random user input.
+
+"""
+if __name__ == '__main__':
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    """
+
+x = 1
+y = 1
+z = 2
+
+
+
+# i between 0 and x
+# j between 0 and y
+# k between 0 and z
+
+i = [item for item in range(0,x+1)]
+j = [item for item in range(0,y+1)]
+k = [item for item in range(0,z+1)]
+
+#ex []
+
+
+
+
