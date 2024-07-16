9d778c10f619a5d12f2cb5844bdbd7071aceb989
Grant Wooley <grantW900@yahoo.com>
2024-07-15 23:39:17 -0500

    Commiting changes to learning file and new web scraper

--- LearningFile.py
+++ LearningFile.py
@@ -23,19 +23,11 @@ if __name__ == '__main__':
 x = 1
 y = 1
 z = 2
+n = 3
 
+# Exvar = [x  for x in range(0,10) if x ,5]
+#Allpermutations = [[i,j,k] for [i,j,k] in range(0,10) if i+j+k != n ]
 
-
-# i between 0 and x
-# j between 0 and y
-# k between 0 and z
-
-i = [item for item in range(0,x+1)]
-j = [item for item in range(0,y+1)]
-k = [item for item in range(0,z+1)]
-
-#ex []
-
-
-
-
+AcceptablePermutations = [[i,j,k] for i in range(0,x+1) for j in range(0,y+1) for k in range(0,z+1) if i+j+k != n]
+AcceptablePermutations.sort()
+print(AcceptablePermutations)
